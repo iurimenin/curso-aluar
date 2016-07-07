66d86f4cd3e82734d8fd09308fb183a81350642e
Iuri Menin <iurimenin@gmail.com>
2016-07-06 22:45:03 -0300

    Finalizando aulas parte 3

--- Aula Alura/MyPlayground.playground/Contents.swift
+++ Aula Alura/MyPlayground.playground/Contents.swift
@@ -2,4 +2,37 @@
 
 import UIKit
 
-var str = "Hello, playground"
+let calorias:Array<Double> = [50.5, 100, 300, 500]
+
+let itens:Array<Double> = []
+
+for var i = 0; i < calorias.count; i++ {
+    print(calorias[i])
+}
+
+for i in 0...3 {
+    print(calorias[i])
+}
+
+for i in calorias {
+    print(i)
+}
+
+func allCalories(calories:Array<Double>) -> Double {
+    var total:Double = 0
+    for c in calorias {
+        total += c
+    }
+    return total
+}
+
+let totalCalorias = allCalories([10.5, 100, 300, 500])
+print(totalCalorias)
+
+var total = 0
+var values = [1,2]
+
+for v in values {
+    total += v
+}
+print(total / values.count)
--- MyPlayground.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-
-let name = "Iuri"
-let felicidade:Int = 5
-
-
